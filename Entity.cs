a3a66b48ca2a110cb424b7bf8ea18c7ec78d9716
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-08-09 02:56:48 +0330

    Initial Definition of Entity Parent Class

---
+++ Entity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace YellowCreatures
+{
+    abstract class Entity
+    {
+        public Position Position { get; set; }
+        public Size Size { get; set; }
+
+        public Entity(Size size)
+        {
+            Size = size;
+        }
+        
+        private List<Position> FindPossiblePositions(List<Entity> currentEntitiesPositions)
+        {
+            List<Position> possiblePositions = new List<Position>();
+
+
+
+            return possiblePositions;
+        }
+    }
+}
