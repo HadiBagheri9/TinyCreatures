a58c8689d36dd3998b967f6700c5a5840c9b21f9
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-08-11 18:44:42 +0330

    Add Model -> Land

---
+++ Models/Land.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TinyCreatures
+{
+    class Land
+    {
+        public Size ScreenSize { get; }
+
+        public Land(Size size, ConsoleColor color)
+        {
+            Console.SetWindowSize(size.Width, (size.Height / 2));
+            Console.BackgroundColor = color;
+            Console.Clear();
+            // Console.SetWindowPosition(3, 15);
+        }
+    }
+}
