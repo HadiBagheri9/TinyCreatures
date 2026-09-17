6f65afa6854fda5e3b9d553f23051126927637fa
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2026-09-18 02:29:16 +0330

    Create file -> Land.cs

---
+++ Land.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TinyCreatures
+{
+    class Land
+    {
+        public Size ScreenSize { get; }
+
+        public Land(Size size, ConsoleColor color)
+        {
+            ScreenSize = size;
+            Console.SetWindowSize(ScreenSize.Width, (ScreenSize.Height / 2));
+            Console.BackgroundColor = color;
+            Console.Clear();
+        }
+    }
+}
